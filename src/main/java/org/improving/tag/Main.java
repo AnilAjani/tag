a7d08745046b1d12d1788c8646b7f38cc4c00829
Anil Ajani <Anil.Ajani@improving.com>
2019-09-20 09:58:34 -0500

    Implemented SpringContext

--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -1,15 +1,17 @@
 package org.improving.tag;
 
-import java.util.Scanner;
+import org.improving.tag.commands.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main
 {
-    public static void main(String[] args){
-        System.out.println("Start of main");
-        Game game = new Game();
-        System.out.println("Declared game");
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringContext.class);
+
+        Game game = context.getBean(Game.class);
         game.run();
-        System.out.println("After run()");
+
     long elapsedTicks = game.getEndTime().getTime() -
             game.getStartTime().getTime();
     double elapsedSeconds = elapsedTicks / 1000.0;
